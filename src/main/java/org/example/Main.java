bb75f83f340ab3b34e74041cf51cb1461619f1c5
Shahina <gozelllhesimovaaa@gmail.com>
2025-06-14 21:03:53 +0400

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a =5;
-        int b = 10;
-        System.out.println(a + b);
+        double a = 5.76675;
+        long b = 7;
+        int c = 3;
+        double result = (a++) + (--b) - (++a) + (a += 3) + (++c);
+
+        System.out.printf("%2.5f", result);
+
+
+        Scanner sc = new Scanner(System.in);
+        int S = sc.nextInt();
+        int K = sc.nextByte();
+        System.out.println(a < b ? "Fəxri" : a > b ? "Tural" : "Murad");
+
+        double s = 32;
+        char g = 's';
+        System.out.println(s+g);
+
+
+
     }
+
 }
