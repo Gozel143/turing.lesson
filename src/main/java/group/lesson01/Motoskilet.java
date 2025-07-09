57db2bade491b3d985433519a5827081745a7fae
Shahina <gozelllhesimovaaa@gmail.com>
2025-07-09 20:58:54 +0400

    tasks

---
+++ src/main/java/group/lesson01/Motoskilet.java
@@ -0,0 +1,22 @@
+package group.lesson01;
+
+public class Motoskilet {
+    public String color;
+    public String model;
+    public String name;
+    public String wheel;
+
+
+    public Motoskilet(String name, String color, String model, String wheel) {
+        this.color = color;
+        this.model = model;
+        this.name = name;
+        this.wheel = wheel;
+    }
+
+    public String toString() {
+        return ("Motoskilet = color:%s\n  model:%s\n name:%s\n wheel:%s\n  ").formatted(this.color, this.model, this.name, this.wheel);
+    }
+}
+
+
---
+++ src/main/java/group/lesson01/MotoskiletApp.java
@@ -0,0 +1,8 @@
+package group.lesson01;
+
+public class MotoskiletApp {
+    public static void main(String[] args) {
+        Motoskilet motoskilet = new Motoskilet("R1M", "Red", "CBR650R", "2");
+        System.out.println(motoskilet.toString());
+    }
+}
